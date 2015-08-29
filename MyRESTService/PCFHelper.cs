2bb7800b117a3885b3713552d636347ad22913e8
mwright-pivotal <mwright@pivotal.io>
2015-08-29 14:47:02 -0400

    added PCF helper

---
+++ MyRESTService/PCFHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace MyRESTService
+{
+    public static class PCFHelper
+    {
+        public static string Bind(string serviceType, string connectionStringName)
+        {
+            Console.WriteLine("Beginning bind for: " + connectionStringName);
+
+            if (ConfigurationManager.ConnectionStrings[connectionStringName] == null)
+            {
+                Console.WriteLine("No connection string found matching given connection string name, returning null...");
+                return null;
+            }
+
+            string vcapServices = Environment.GetEnvironmentVariable("VCAP_SERVICES");
+            Console.WriteLine("Detected VCAP_SERVICES value of: " + vcapServices);
+
+            if (String.IsNullOrEmpty(vcapServices))
+            {
+                Console.WriteLine("VCAP_SERVICES not set - returning connection string from app config...");
+                return ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+            }
+
+            JObject root = JObject.Parse(vcapServices);
+            var cfServiceRoot = root[serviceType];
+            if (cfServiceRoot == null)
+            {
+                throw new ArgumentException("The specified service type could not be found in VCAP_SERVICES.",
+                    serviceType);
+            }
+            Console.WriteLine("Retrieved service information for " + serviceType);
+
+            var cfServiceInstance =
+                cfServiceRoot.FirstOrDefault(si => Extensions.Value<string>(si["name"]) == connectionStringName);
+            if (cfServiceInstance == null)
+            {
+                throw new ArgumentException(
+                    "The specified service name could not be found in VCAP_SERVICES. Was the service broker with the correct name created?",
+                    connectionStringName);
+            }
+            Console.WriteLine("Retrieved service instance information for " + connectionStringName);
+
+            var connectionString = cfServiceInstance["credentials"]["connection"].Value<string>();
+            Console.WriteLine("Replacing connection string information for " + connectionStringName + " with VCAP_SERVICES value of " + connectionString);
+
+            return connectionString;
+        }
+    }
+}
---
+++ MyRESTService/ProductRESTService.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace MyRESTService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductRESTService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ProductRESTService.svc or ProductRESTService.svc.cs at the Solution Explorer and start debugging.
+    public class ProductRESTService : IProductRESTService
+    {
+        public List<Product> GetProductList()
+        {
+            return Products.Instance.ProductList;
+        }
+
+        public void SendMessage(String msg)
+        {
+            var channel = this.getChannel();
+        }
+
+        public RabbitMQ.Client.IModel getChannel()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            return connection.CreateModel();
+        }
+    }
+}
